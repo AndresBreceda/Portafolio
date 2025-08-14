6fbca00865770fe956a63f6840893ff642efd07b
Andrés Breceda <a.e.breceda@gmail.com>
2025-08-14 17:41:50 -0600

    primeros pasos landing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,22 @@
+import { AlignJustify } from "lucide-react";
 
 export default function Header(){
-    return(<div>Header</div>);
+    return(
+        <header className="flex justify-between items-center p-10">
+            <div className="flex items-center gap-3">
+                <img 
+                    src="./andres-logo.svg" 
+                    alt="Logo Andrés Portafolio" 
+                    className="h-10 w-auto"
+                />
+                <div className="text-2xl font-semibold">
+                    Andrés E. Breceda
+                </div>
+            </div>
+
+            <div>
+                <AlignJustify className="w-6 h-6" />
+            </div>
+        </header>
+    );
 }
--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,4 +1,31 @@
-
 export default function Landing(){
-    return(<div>Landing</div>);
+    return(
+        <div className="bg-white flex justify-between items-center p-10 m-10 h-screen gap-10 border-8 border-black"
+             style={{ borderStyle: "double" }}>
+            
+            {/* Contenedor de la imagen */}
+            <div className="flex-1 flex justify-center items-center">
+                <img 
+                    src="./andres-logo.webp" 
+                    alt="imagen de andres"
+                    className="max-w-full h-auto object-contain"
+                />
+            </div>
+
+            {/* Contenedor del texto */}
+            <div className="flex-1 flex flex-col justify-center">
+                <p className="text-2xl mb-4">
+                    Hola soy <strong>Andrés E. Breceda</strong>
+                </p>
+                <p className="text-4xl font-bold mb-4">
+                    Desarrollador Web Full-stack
+                </p>
+                <p className="text-2xl leading-relaxed">
+                    Tengo un año de experiencia en desarrollo web, además de trabajar como profesor 
+                    de inglés, portugués y francés.
+                </p>
+            </div>
+
+        </div>
+    );
 }
