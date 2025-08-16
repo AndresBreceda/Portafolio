f74da579110be204b968e31882e935935fa7089c
Andrés Breceda <a.e.breceda@gmail.com>
2025-08-16 10:49:31 -0600

    footert primeros pasos

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,44 @@
+import { GithubIcon, InstagramIcon, Linkedin, Mail } from "lucide-react";
 
 export default function Footer(){
-    return(<div>Footer</div>);
+    return(
+    <footer className="bg-black flex justify-between p-10 text-white">
+        <div>
+            {/* cambiar a logo en blanco */}
+            <img 
+                src="./andres-logo.svg" 
+                alt="Logo Andrés Portafolio" 
+                className="h-10 w-auto"
+            />
+            <div className="text-2xl font-semibold">
+                Andrés E. Breceda
+            </div>
+        </div>
+
+        <div>
+            <h2 className="text-2xl"><strong>Contactame</strong></h2>
+            <div className="flex gap-4 p-4">
+                <a className="hover:text-yellow-400" href="https://www.instagram/AndresBreceda.com" target="_blank">
+                    <InstagramIcon></InstagramIcon>
+                </a>
+                
+                <a className="hover:text-yellow-400" href="https://www.instagram/AndresBreceda.com" target="_blank">
+                    <Linkedin></Linkedin>
+                </a>
+                    
+                <a className="hover:text-yellow-400" href="https://www.instagram/AndresBreceda.com" target="_blank">
+                    <Mail></Mail>
+                </a>
+                    
+                <a className="hover:text-yellow-400" href="https://www.instagram/AndresBreceda.com" target="_blank">
+                    <GithubIcon></GithubIcon>                    
+                </a>
+
+            </div>
+        </div>
+        
+
+    </footer>
+
+    );
 }
